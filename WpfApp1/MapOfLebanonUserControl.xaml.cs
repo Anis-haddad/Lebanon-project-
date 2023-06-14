5c8e665264fcaf71048b5249fb316f22b79674f8
lrodak <lukasz.rodak.prv@gmail.com>
2023-06-14 14:20:38 +0200

    Map of Lebanon Cities

--- WpfApp1/MapOfLebanonUserControl.xaml.cs
+++ WpfApp1/MapOfLebanonUserControl.xaml.cs
@@ -23,6 +23,144 @@ namespace WpfApp1
         public MapOfLebanonUserControl()
         {
             InitializeComponent();
+            Grid.SetZIndex(MapOfLebanon, int.MinValue);
+        }
+
+        private void ByblosMapElipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //ByblosMapElipse.Visibility = Visibility.Visible;
+            ByblosMapElipse.Fill = new SolidColorBrush(Colors.DarkBlue);
+            
+        }
+
+        private void ByblosMapElipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //ByblosMapElipse.Visibility = Visibility.Hidden;
+            ByblosMapElipse.Fill = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void BeirutMapElipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BeirutMapElipse.Fill = new SolidColorBrush(Colors.DarkBlue);
+        }
+
+        private void BeirutMapElipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            BeirutMapElipse.Fill = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void BaalbakMapElipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BaalbakMapElipse.Fill = new SolidColorBrush(Colors.DarkBlue);
+        }
+
+        private void BaalbakMapElipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            BaalbakMapElipse.Fill = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void HarissaMapElipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            HarissaMapElipse.Fill = new SolidColorBrush(Colors.DarkBlue);
+        }
+
+        private void HarissaMapElipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            HarissaMapElipse.Fill = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void FarayaMapElipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            FarayaMapElipse.Fill = new SolidColorBrush(Colors.DarkBlue);
+        }
+
+        private void FarayaMapElipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            FarayaMapElipse.Fill = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void ByblosMapElipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ByblosMapElipse.Fill = new SolidColorBrush(Colors.Red);
+
+            BeirutisACity.Visibility = Visibility.Collapsed;
+            MenuMainCities.Visibility = Visibility.Collapsed;
+            HarrissaCityIsACity.Visibility = Visibility.Collapsed;
+            FarayaCityIsACity.Visibility = Visibility.Collapsed;
+            ByblosCityIsACity.Visibility = Visibility.Visible;
+            BaalbakCityIsaCity.Visibility = Visibility.Collapsed;
+            MapOfLebanon.Visibility = Visibility.Hidden;
+            GoBackFromMainCities.Visibility = Visibility.Visible;
+        }
+
+        private void BaalbakMapElipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            BaalbakMapElipse.Fill = new SolidColorBrush(Colors.Red);
+
+            BeirutisACity.Visibility = Visibility.Collapsed;
+            MenuMainCities.Visibility = Visibility.Collapsed;
+            HarrissaCityIsACity.Visibility = Visibility.Collapsed;
+            FarayaCityIsACity.Visibility = Visibility.Collapsed;
+            ByblosCityIsACity.Visibility = Visibility.Collapsed;
+            BaalbakCityIsaCity.Visibility = Visibility.Visible;
+            MapOfLebanon.Visibility = Visibility.Hidden;
+            GoBackFromMainCities.Visibility = Visibility.Visible;
+        }
+
+        private void BeirutMapElipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            BeirutMapElipse.Fill = new SolidColorBrush(Colors.Red);
+            BeirutisACity.Visibility = Visibility.Visible;
+            MenuMainCities.Visibility = Visibility.Collapsed;
+            HarrissaCityIsACity.Visibility = Visibility.Collapsed;
+            FarayaCityIsACity.Visibility = Visibility.Collapsed;
+            ByblosCityIsACity.Visibility = Visibility.Collapsed;
+            BaalbakCityIsaCity.Visibility = Visibility.Collapsed;
+            MapOfLebanon.Visibility = Visibility.Hidden;
+            GoBackFromMainCities.Visibility = Visibility.Visible;
+        }
+
+        private void HarissaMapElipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HarissaMapElipse.Fill = new SolidColorBrush(Colors.Red);
+            BeirutisACity.Visibility = Visibility.Collapsed;
+            MenuMainCities.Visibility = Visibility.Collapsed;
+            HarrissaCityIsACity.Visibility = Visibility.Visible;
+            FarayaCityIsACity.Visibility = Visibility.Collapsed;
+            ByblosCityIsACity.Visibility = Visibility.Collapsed;
+            BaalbakCityIsaCity.Visibility = Visibility.Collapsed;
+            MapOfLebanon.Visibility = Visibility.Hidden;
+            GoBackFromMainCities.Visibility = Visibility.Visible;
+        }
+
+        private void FarayaMapElipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            FarayaMapElipse.Fill = new SolidColorBrush(Colors.Red);
+            BeirutisACity.Visibility = Visibility.Collapsed;
+            MenuMainCities.Visibility = Visibility.Collapsed;
+            HarrissaCityIsACity.Visibility = Visibility.Collapsed;
+            FarayaCityIsACity.Visibility = Visibility.Visible;
+            ByblosCityIsACity.Visibility = Visibility.Collapsed;
+            BaalbakCityIsaCity.Visibility = Visibility.Collapsed;
+            MapOfLebanon.Visibility = Visibility.Hidden;
+            GoBackFromMainCities.Visibility = Visibility.Visible;
+        }
+
+        private void GoBackFromMainCities_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void GoBackFromMainCities_Click(object sender, RoutedEventArgs e)
+        {
+            BeirutisACity.Visibility = Visibility.Hidden;
+            MenuMainCities.Visibility = Visibility.Hidden;
+            HarrissaCityIsACity.Visibility = Visibility.Hidden;
+            FarayaCityIsACity.Visibility = Visibility.Hidden;
+            ByblosCityIsACity.Visibility = Visibility.Hidden;
+            BaalbakCityIsaCity.Visibility = Visibility.Hidden;
+            MapOfLebanon.Visibility = Visibility.Visible;
+            GoBackFromMainCities.Visibility = Visibility.Hidden;
         }
     }
 }
